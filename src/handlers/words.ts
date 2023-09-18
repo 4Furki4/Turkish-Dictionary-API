b3b19ea04e9d6fb868fac63943bcf3e267bb784d
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-18 22:15:54 +0300

    fix: addWord returns the whole word including meanings

--- src/handlers/words.ts
+++ src/handlers/words.ts
@@ -69,6 +69,9 @@ export async function addWord(context: handlerContext) {
           },
         },
       },
+      include: {
+        meanings: true,
+      },
     });
     context.set.status = 201;
     return { data: createdWord };
